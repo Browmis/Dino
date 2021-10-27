002ac4839500574b970edf0ada0a42488bc2b07c
Browmis <bmisha85@gmail.com>
2021-10-27 21:39:34 +0200

    add onClickListener, and layout for play window

--- app/src/main/java/com/example/dinogame/MainActivity.java
+++ app/src/main/java/com/example/dinogame/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.dinogame;
 
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.horizontal_main);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        Button playBt = (Button) findViewById(R.id.button);
+        playBt.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.button:
+                setContentView(R.layout.play_layout);
+                setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+                break;
+        }
     }
 }
